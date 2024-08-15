16582688cf3bf01347dea18204374bebbe11a897
emrafi <mr.firmanssyah@gmail.com>
2024-08-15 20:26:02 +0700

    add meta insecure request

--- resources/views/frontend/pantau.blade.php
+++ resources/views/frontend/pantau.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Karanggintung | Pantau</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
